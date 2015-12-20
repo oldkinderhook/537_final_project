a2f0afb682e4397992e393f78263a9e01a3df3e8
oldkinderhook <scottkith@gmail.com>
2015-12-20 18:03:51 -0500

    Added Show View and Modified Controllers

--- scott_thompson_final_project/app/controllers/articles_controller.rb
+++ scott_thompson_final_project/app/controllers/articles_controller.rb
@@ -5,28 +5,31 @@ except: [:index, :show]
   def index
     @articles = Article.all
   end
-
+=end
   def show
     @article = Article.find(params[:id])
   end
-=end
+
   def new
-    #@article = Article.new
+    @article = Article.new
   end
 =begin
   def edit
     @article = Article.find(params[:id])
   end
+=end
 
   def create
     @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
-    else
-      render 'new'
+    #else
+    #  render 'new'
     end
   end
+
+=begin
 
   def update
     @article = Article.find(params[:id])
@@ -44,10 +47,10 @@ except: [:index, :show]
     @article.destroy
     redirect_to articles_path
   end
-
+=end
   private
     def article_params
       params.require(:article).permit(:title, :text)
     end
-=end
+
 end
